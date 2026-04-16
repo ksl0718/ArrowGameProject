e9b97ad68c7865d24fe0046c0cbeb81ae88b56e6
SEUNGRAEM <ksl0718@nate.com>
2026-04-16 23:35:17 +0900

    fix - 호스트/클라이언트 분신 이동시 블랜드 스페이스 다르게 작용하던 부분 수정

--- Source/ArrowGame/Character/UserCharacterAnimInstance.cpp
+++ Source/ArrowGame/Character/UserCharacterAnimInstance.cpp
@@ -10,6 +10,7 @@
 #include "Kismet/KismetMathLibrary.h" // 방향 계산용 수학 함수
 #include "KismetAnimationLibrary.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "../Character/DokkaebiDecoy.h"
 
 void UUserCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
@@ -28,6 +29,21 @@ void UUserCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
     else
     {
         bIsDead = false;
+    }
+
+    if (ADokkaebiDecoy* Decoy = Cast<ADokkaebiDecoy>(Pawn))
+    {
+        // Decoy는 네트워크 보간 영향 줄이려고 Velocity 기반만 사용
+        const float RawSpeed = Character->GetVelocity().Size2D();
+        // 이동 중이면 목표 속도를 DecoySpeed로 고정해 호스트/클라 체감 맞춤
+        const float TargetSpeed = (RawSpeed > 5.f) ? Decoy->DecoySpeed : 0.f;
+        // 시각 스무딩
+        GroundSpeed = FMath::FInterpTo(GroundSpeed, TargetSpeed, DeltaSeconds, 10.f);
+        // 직진 분신이면 방향값은 0 고정해도 충분
+        Direction = 0.f;
+        bShouldMove = (GroundSpeed > 3.f);
+        // Decoy는 여기서 끝. 아래 플레이어 전용 로직 안 타게 종료.
+        return;
     }
 
     const FVector Velocity = Character->GetVelocity();
