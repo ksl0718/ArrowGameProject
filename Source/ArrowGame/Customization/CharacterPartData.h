56d1d31493f21b13df77bc35d5916ba8500cf37b
SEUNGRAEM <ksl0718@nate.com>
2026-08-30 15:32:40 +0900

    add - 커스터마이징 파츠 데이터 에셋 추가

---
+++ Source/ArrowGame/Customization/CharacterPartData.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterPartData.h"
+#include "CharacterPartData.generated.h"
+
+class USkeletalMesh;
+class UTexture2D;
+
+UCLASS(BlueprintType)
+class ARROWGAME_API UCharacterPartData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+		
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Customization")
+	FText DisplayName;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Customization")
+	ECustomizeSlot Slot = ECustomizeSlot::Hair;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Customization")
+	EPartAttachMode AttachMode = EPartAttachMode::RootLeaderPose;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Customization")
+	TSoftObjectPtr<USkeletalMesh> Mesh;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Customization|UI")
+	TSoftObjectPtr<UTexture2D> Icon;
+	
+};
