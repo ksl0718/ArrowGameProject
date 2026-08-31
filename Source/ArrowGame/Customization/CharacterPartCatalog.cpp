394cb7b8f15cbab4ec80d2a217f463de9f50d8fd
SEUNGRAEM <ksl0718@nate.com>
2026-08-31 23:48:33 +0900

    feat - 커스터마이징 파츠 카탈로그 추가

---
+++ Source/ArrowGame/Customization/CharacterPartCatalog.cpp
@@ -0,0 +1,16 @@
+﻿#include "CharacterPartCatalog.h"
+#include "CharacterPartData.h"
+
+void UCharacterPartCatalog::GetPartsBySlot(ECustomizeSlot Slot, TArray<UCharacterPartData*>& OutParts) const
+{
+	OutParts.Reset();
+	
+	for (const TSoftObjectPtr<UCharacterPartData> PartRef : Parts)
+	{
+		UCharacterPartData* Part = PartRef.LoadSynchronous();
+		if (Part && Part->Slot == Slot)
+		{
+			OutParts.Add(Part);
+		}
+	}
+}
---
+++ Source/ArrowGame/Customization/CharacterPartCatalog.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterCustomizeTypes.h"
+#include "CharacterPartCatalog.generated.h"
+
+class UCharacterPartData;
+
+UCLASS(BlueprintType)
+class ARROWGAME_API UCharacterPartCatalog : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Customization")
+	TArray<TSoftObjectPtr<UCharacterPartData>> Parts;
+	
+	UFUNCTION(BlueprintCallable, Category = "Customization")
+	void GetPartsBySlot(ECustomizeSlot Slot, TArray<UCharacterPartData*>& OutParts) const;
+	
+};
